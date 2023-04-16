54ca70a725ba8a49e0d3e60f2d18956b3daa386e
jiyeon <sjy6736@naver.com>
2023-04-17 02:34:53 +0900

    feat: 2차 세미나 init commit

---
+++ SecondSeminar/src/main/java/sopt/org/SecondSeminar/SecondSeminarApplication.java
@@ -0,0 +1,23 @@
+package sopt.org.SecondSeminar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import sopt.org.SecondSeminar.basic.domain.post.Post;
+import sopt.org.SecondSeminar.basic.domain.user.User;
+
+import java.util.ArrayList;
+
+@SpringBootApplication
+public class SecondSeminarApplication {
+
+	public static ArrayList<User> userList;
+	public static ArrayList<Post> postList;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSeminarApplication.class, args);
+
+		userList = new ArrayList<>();
+		postList = new ArrayList<>();
+	}
+
+}
