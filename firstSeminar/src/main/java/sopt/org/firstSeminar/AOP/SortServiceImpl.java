b0f93224cb3321adee9b0201e528fe489717f8d6
jiyeon <sjy6736@naver.com>
2023-04-18 00:31:22 +0900

    basic: AOP관련 실습

---
+++ firstSeminar/src/main/java/sopt/org/firstSeminar/AOP/SortServiceImpl.java
@@ -0,0 +1,8 @@
+package sopt.org.firstSeminar.AOP;
+
+public class SortServiceImpl implements SortService {
+
+    public void sort() {
+        //정렬해줌
+    }
+}
---
+++ firstSeminar/src/main/java/sopt/org/firstSeminar/AOP/Sorter.java
@@ -0,0 +1,6 @@
+package sopt.org.firstSeminar.AOP;
+
+public interface Sorter {
+
+    void Sort();
+}
