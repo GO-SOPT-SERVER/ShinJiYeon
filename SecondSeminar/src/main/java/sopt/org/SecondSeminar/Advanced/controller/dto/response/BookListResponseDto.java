70c410ab5bb189b2b62b1372db8158ff5e91d8be
jiyeon <sjy6736@naver.com>
2023-04-19 20:53:13 +0900

    bookList를 dto로 바꾸는 부분 Stream을 사용하여 직관적으로 수정

--- SecondSeminar/src/main/java/sopt/org/SecondSeminar/Advanced/controller/dto/response/BookListResponseDto.java
+++ SecondSeminar/src/main/java/sopt/org/SecondSeminar/Advanced/controller/dto/response/BookListResponseDto.java
@@ -5,13 +5,13 @@ import sopt.org.SecondSeminar.Advanced.domain.Book;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class BookListResponseDto {
     List<BookInfoResponseDto> bookInfoList;
 
     public BookListResponseDto(List<Book> bookList) {
-        bookInfoList = new ArrayList<>();
-        bookList.forEach(book -> bookInfoList.add(new BookInfoResponseDto(book)));
+        bookInfoList = bookList.stream().map(BookInfoResponseDto::new).collect(Collectors.toList());
     }
 }
